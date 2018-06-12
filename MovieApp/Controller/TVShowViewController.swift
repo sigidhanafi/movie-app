311e9e6c77f2c9148c560dd542280dd8a9b46367
sigidhanafi <sigidhanafi@gmail.com>
2018-06-12 15:49:38 +0700

    add tabbar and view controller

---
+++ MovieApp/Controller/TVShowViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TVShowViewController.swift
+//  MovieApp
+//
+//  Created by Sigit Hanafi on 6/12/18.
+//  Copyright © 2018 Sigit Hanafi. All rights reserved.
+//
+
+import UIKit
+
+class TVShowViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "TV Shows"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
