f6171d76caccc065b5ce8c02bb2babbb27666e31
sigidhanafi <sigidhanafi@gmail.com>
2018-06-13 23:19:00 +0700

    change extension

--- MovieApp/View/MovieCardViewCell.swift
+++ MovieApp/View/MovieCardViewCell.swift
@@ -17,7 +17,6 @@ class MovieCardViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-
 }
 
 extension MovieCardViewCell: UICollectionViewDataSource {
