01e66d0230defdcca7b6a0e3a2245277ccd7b234
sigidhanafi <sigidhanafi@gmail.com>
2018-06-13 22:26:23 +0700

    add tableview with collectionview

--- MovieApp/Controller/MovieViewController.swift
+++ MovieApp/Controller/MovieViewController.swift
@@ -8,19 +8,38 @@
 
 import UIKit
 
-class MovieViewController: UIViewController {
-
+class MovieViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var sections = [String]()
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Movies"
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        sections = ["Popular", "Action", "Drama", "Kids", "Horor"]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
     }
-
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCardCell", for: indexPath)
+        return cell
+    }
 
 }
 
---
+++ MovieApp/View/MovieCardViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  MovieCardViewCell.swift
+//  MovieApp
+//
+//  Created by Sigit Hanafi on 6/13/18.
+//  Copyright © 2018 Sigit Hanafi. All rights reserved.
+//
+
+import UIKit
+
+class MovieCardViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
+
+extension MovieCardViewCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCardCollectionView", for: indexPath) as! UICollectionViewCell
+        return cell
+    }
+}
+
+extension MovieCardViewCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemsPerRow:CGFloat = 4
+        let hardCodedPadding:CGFloat = 5
+        let itemWidth = (collectionView.bounds.width / itemsPerRow) - hardCodedPadding
+        let itemHeight = collectionView.bounds.height - (2 * hardCodedPadding)
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+}
