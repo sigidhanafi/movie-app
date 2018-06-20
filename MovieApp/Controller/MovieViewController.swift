05cbf588aaac47835ff42a0050cf5a334cb70fd9
sigidhanafi <sigidhanafi@gmail.com>
2018-06-20 22:35:15 +0700

    change tableview to UIScrollView

--- MovieApp/Controller/MovieViewController.swift
+++ MovieApp/Controller/MovieViewController.swift
@@ -8,49 +8,32 @@
 
 import UIKit
 
-class MovieViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MovieViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var stackView: UIStackView!
     
     var sections = [String]()
-    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Movies"
         
-        tableView.dataSource = self
-        tableView.delegate = self
-        
         sections = ["Popular", "Action", "Drama", "Kids", "Horor"]
-    }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return sections.count
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section]
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let sectionTitle = sections[section]
+        let view1 = Bundle.main.loadNibNamed("View", owner: self, options: nil)?.first as! UIView
+        self.stackView.addArrangedSubview(view1)
+        view1.translatesAutoresizingMaskIntoConstraints = false
+        view1.heightAnchor.constraint(equalToConstant: 200).isActive = true
         
-        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 40))
-        let label = UILabel(frame: CGRect(x: 10, y: 0, width: tableView.bounds.width, height: 40))
-        label.textColor = UIColor.white
-        label.text = sectionTitle
-        label.font = UIFont(name: "HelveticaNeue-Bold", size: 16)
-        view.addSubview(label)
-        return view
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCardCell", for: indexPath)
-        return cell
+        let view2 = UIView()
+        view2.backgroundColor = UIColor.blue
+        self.stackView.addArrangedSubview(view2)
+        view2.translatesAutoresizingMaskIntoConstraints = false
+        view2.heightAnchor.constraint(equalToConstant: 200).isActive = true
+
+        
     }
 
 }
