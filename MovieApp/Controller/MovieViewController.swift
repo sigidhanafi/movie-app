522a871fbe65334e48fa58e5481d8f455f990e71
sigidhanafi <sigidhanafi@gmail.com>
2018-06-22 00:39:53 +0700

    collection view with image and label from data array

--- MovieApp/Controller/MovieViewController.swift
+++ MovieApp/Controller/MovieViewController.swift
@@ -14,14 +14,32 @@ class MovieViewController: UIViewController {
     @IBOutlet weak var stackView: UIStackView!
     
     var sections = [String]()
+    var movie = [String: String]()
+    var movies = [[String: String]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Movies"
         
-        sections = ["Popular", "Action", "Drama", "Kids", "Horor"]
+        let movie1: [String: String] = ["title": "Jurassic Park", "image": "jurassic-park"]
+        movies.append(movie1)
+        let movie2: [String: String] = ["title": "Jurassic World", "image": "jurassic-world"]
+        movies.append(movie2)
+        let movie3: [String: String] = ["title": "The Lost World", "image": "the-lost-world"]
+        movies.append(movie3)
+        let movie4: [String: String] = ["title": "Jurassic Park", "image": "jurassic-park"]
+        movies.append(movie4)
+        let movie5: [String: String] = ["title": "Jurassic World", "image": "jurassic-world"]
+        movies.append(movie5)
+        let movie6: [String: String] = ["title": "The Lost World", "image": "the-lost-world"]
+        movies.append(movie6)
     
+        self.setupView()
+        
+    }
+    
+    func setupView() {
         let view1 = Bundle.main.loadNibNamed("View", owner: self, options: nil)?.first as! UIView
         self.stackView.addArrangedSubview(view1)
         view1.backgroundColor = UIColor.cyan
@@ -50,20 +68,18 @@ class MovieViewController: UIViewController {
         self.stackView.addArrangedSubview(view2)
         view2.translatesAutoresizingMaskIntoConstraints = false
         view2.heightAnchor.constraint(equalToConstant: 250).isActive = true
-
+        
         let view3 = UIView()
         view3.backgroundColor = UIColor.white
         self.stackView.addArrangedSubview(view3)
         view3.translatesAutoresizingMaskIntoConstraints = false
         view3.heightAnchor.constraint(equalToConstant: 250).isActive = true
-
+        
         let view4 = UIView()
         view4.backgroundColor = UIColor.cyan
         self.stackView.addArrangedSubview(view4)
         view4.translatesAutoresizingMaskIntoConstraints = false
         view4.heightAnchor.constraint(equalToConstant: 250).isActive = true
-
-        
     }
 
 }
@@ -75,11 +91,20 @@ extension MovieViewController: UICollectionViewDelegate, UICollectionViewDataSou
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 8
+        return movies.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCell", for: indexPath) as! MovieCollectionViewCell
+        
+        if let title = movies[indexPath.row]["title"] {
+            cell.titleLabel.text = title
+        }
+        
+        if let image = movies[indexPath.row]["image"] {
+            cell.coverImage.image = UIImage(named: "\(image)")
+        }
+        
         return cell
     }
     
--- MovieApp/View/MovieCollectionViewCell.swift
+++ MovieApp/View/MovieCollectionViewCell.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class MovieCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var coverImage: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
